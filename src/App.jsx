17f3413f40f6be499e4920309cdf641ea2159cc8
Jeremy Smeltz <smeltzjeremy1@gmail.com>
2026-06-17 14:33:53 -0400

    fix: inject global root style reset to eliminate white canvas window borders

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,36 @@ import UIOverlay from './components/UIOverlay';
 
 export default function App() {
   return (
-    <div style={{ 
-      width: '100vw', 
-      height: '100vh', 
-      position: 'relative', 
-      margin: 0, 
-      padding: 0,
-      overflow: 'hidden'
-    }}>
-      {/* 3D Render Layer */}
-      <SceneContainer />
-      
-      {/* User Interface Interaction Layer */}
-      <UIOverlay />
-    </div>
+    <>
+      {/* Global CSS Injector to smash browser margins, padding, and layout borders */}
+      <style>{`
+        * {
+          box-sizing: border-box;
+        }
+        html, body, #root {
+          margin: 0 !important;
+          padding: 0 !important;
+          width: 100vw !important;
+          height: 100vh !important;
+          overflow: hidden !important;
+          background-color: #080b11;
+        }
+      `}</style>
+
+      <div style={{ 
+        width: '100vw', 
+        height: '100vh', 
+        position: 'relative', 
+        margin: 0, 
+        padding: 0,
+        overflow: 'hidden'
+      }}>
+        {/* 3D Render Layer */}
+        <SceneContainer />
+        
+        {/* User Interface Interaction Layer */}
+        <UIOverlay />
+      </div>
+    </>
   );
 }
