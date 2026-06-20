1acbc78bb2007801b2b9a7e1323465e6a37908a3
Jeremy Smeltz <smeltzjeremy1@gmail.com>
2026-06-20 17:23:41 -0400

    style: apply standard material roughness limits and adjust canvas fov scaling

--- src/components/3d/InteractiveGalleryWheel.jsx
+++ src/components/3d/InteractiveGalleryWheel.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 export default function InteractiveGalleryWheel() {
   return (
     <group>
-      {/* 🌊 LAYER 1: SOLID CHROME BACKDROP STABILIZER */}
+      {/* 🌊 LAYER 1: ROBUST HIGH-SHINE METAL STANDARD PLANE */}
       <mesh position={[0, 0, -12]} rotation={[-Math.PI / 3, 0, 0]}>
         <planeGeometry args={[100, 100]} />
-        <meshPhysicalMaterial 
+        <meshStandardMaterial 
           color="#0a0a0f" 
           metalness={1.0} 
-          roughness={0.05} 
-          clearcoat={1.0} 
+          roughness={0.1} // Smoothly disperses the harsh light spot
         />
       </mesh>
     </group>
