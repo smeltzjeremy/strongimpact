e05e11537e06c0311fb9ea37f296cb84454b041e
Jeremy Smeltz <smeltzjeremy1@gmail.com>
2026-06-20 17:09:46 -0400

    style: reposition chrome backdrop to z -12 to remove specular light overexposure

--- src/components/3d/InteractiveGalleryWheel.jsx
+++ src/components/3d/InteractiveGalleryWheel.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 export default function InteractiveGalleryWheel() {
   return (
     <group>
-      {/* 🌊 LAYER 1: BASIC HIGH-SHINE PLANE (RELIABLE COVERAGE) */}
-      <mesh position={[0, 0, -10]} rotation={[-Math.PI / 3, 0, 0]}>
+      {/* 🌊 LAYER 1: DEEP HIGH-SHINE PLANE (ANTI-OVEREXPOSURE RESET) */}
+      <mesh position={[0, 0, -12]} rotation={[-Math.PI / 3, 0, 0]}>
         <planeGeometry args={[100, 100]} />
         <meshPhysicalMaterial 
           color="#0a0a0f" 
           metalness={1.0} 
           roughness={0.05} 
           clearcoat={1.0} 
+          clearcoatRoughness={0.0} 
         />
       </mesh>
     </group>
