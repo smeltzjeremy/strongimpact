19b979568a5836fb78dd5ed647d63e0edf3c6589
Jeremy Smeltz <smeltzjeremy1@gmail.com>
2026-06-24 16:24:58 -0400

    Add metallic spokes and proper photo frames

--- src/components/PhotoWheel.tsx
+++ src/components/PhotoWheel.tsx
@@ -28,28 +28,47 @@ const PhotoWheel: React.FC = () => {
     }
   });
 
-  const brightMat = new THREE.MeshBasicMaterial({ color: '#ff00ff' });
-  const frameMat = new THREE.MeshBasicMaterial({ color: '#00ffff' });
+  const spokeMat = new THREE.MeshBasicMaterial({ color: '#dddddd' });
+  const frameMat = new THREE.MeshBasicMaterial({ color: '#1a1a1c' });
+  const photoMat = new THREE.MeshBasicMaterial({ color: '#0f0f11' });
 
-  const radius = 4;
+  const radius = 3.8;   // closer to center
+  const numFrames = 6;
 
   return (
     <>
-      <ambientLight intensity={1.5} />
-      <directionalLight position={[0, 5, 5]} intensity={2.0} />
+      <ambientLight intensity={1.2} />
+      <directionalLight position={[0, 5, 5]} intensity={1.8} />
 
-      <group ref={groupRef} position={[0, 2.2, -2.0]}>
-        <mesh material={brightMat}>
-          <sphereGeometry args={[0.8]} />
+      <group ref={groupRef} position={[0, 2.0, -2.0]}>
+        {/* Center hub */}
+        <mesh material={new THREE.MeshBasicMaterial({ color: '#aaaaaa' })}>
+          <sphereGeometry args={[0.45]} />
         </mesh>
 
-        {Array.from({ length: 6 }).map((_, i) => {
-          const angle = (i * Math.PI * 2) / 6;
+        {Array.from({ length: numFrames }).map((_, i) => {
+          const angle = (i * Math.PI * 2) / numFrames;
           return (
             <group key={i} rotation={[0, 0, angle]}>
-              <mesh position={[0, radius, 0]} material={frameMat}>
-                <boxGeometry args={[2.8, 2.0, 0.3]} />
+              {/* Thin metallic spoke */}
+              <mesh
+                position={[0, radius * 0.52, 0]}
+                rotation={[Math.PI / 2, 0, 0]}
+                material={spokeMat}
+              >
+                <cylinderGeometry args={[0.06, 0.06, radius * 1.05, 16]} />
               </mesh>
+
+              {/* Photo Frame */}
+              <group position={[0, radius, 0]}>
+                <mesh material={frameMat}>
+                  <boxGeometry args={[2.4, 1.75, 0.18]} />
+                </mesh>
+                {/* Photo placeholder */}
+                <mesh position={[0, 0, 0.1]} material={photoMat}>
+                  <planeGeometry args={[2.2, 1.55]} />
+                </mesh>
+              </group>
             </group>
           );
         })}
